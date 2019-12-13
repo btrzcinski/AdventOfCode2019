65369174dd1ddff46e25a910f590ad65d950a913
Barnett Trzcinski <btrzcinski@google.com>
2019-12-13 10:57:14 -0800

    Day 2 Part 2

--- day2-1/cc/main.cc
+++ day2-1/cc/main.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <optional>
 #include <vector>
 
 enum class Opcode : int {
@@ -91,16 +92,43 @@ void print_memory(const ProgramMemory& memory) {
   std::cout << memory[i] << std::endl;
 }
 
+void supply_inputs(ProgramMemory* memory, int noun, int verb) {
+  (*memory)[1] = noun;
+  (*memory)[2] = verb;
+}
+
 void restore_1202_program_alarm_state(ProgramMemory* memory) {
-  (*memory)[1] = 12;
-  (*memory)[2] = 2;
+  supply_inputs(memory, 12, 2);
+}
+
+std::optional<std::pair<int, int>>
+search_for_output(const ProgramMemory& memory_template,
+                  int expected_output) {
+  for (int noun = 0; noun < 1000; ++noun) {
+    for (int verb = 0; verb < 1000; ++verb) {
+      ProgramMemory memory = memory_template;
+      supply_inputs(&memory, noun, verb);
+      run_program(&memory);
+      if (memory.front() == expected_output) {
+        return std::make_optional(std::pair<int, int>{noun, verb});
+      }
+    }
+  }
+
+  return std::nullopt;
 }
 
 int main(int argc, char *argv[]) {
   auto memory = initialize_memory();
-  restore_1202_program_alarm_state(memory.get());
-  run_program(memory.get());
-  print_memory(*memory);
+  auto inputs = search_for_output(*memory, 19690720);
+  if (!inputs.has_value()) {
+    std::cout << "Valid inputs not found." << std::endl;
+    return 1;
+  }
+
+  int noun, verb;
+  std::tie(noun, verb) = inputs.value();
+  std::cout << "noun = " << noun << ", verb = " << verb << std::endl;
 
   return 0;
 }
